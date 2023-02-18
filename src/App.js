9f4ff0084d1457c677743d46f34eec4c5c42cf96
Joel Brauchla <107895300+Joel-AmoebaMan@users.noreply.github.com>
2023-02-18 05:25:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <img src={"banner1.jpeg"}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload....bitchess
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit <code>src/App.js</code> and save to reload...</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React </a>
       </header>
     </div>
   );
